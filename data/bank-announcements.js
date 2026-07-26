ed6f72f3689e18bc592708f0536bdc59ffa8f4bc
vmja2004-ux <vmja2004@gmail.com>
2026-07-26 08:03:31 +0800

    Add data/bank-announcements.js

---
+++ data/bank-announcements.js
@@ -0,0 +1,171 @@
+window.CB_BANK_ANNOUNCEMENTS = {
+  generated_at: "2026-07-26T07:57:53+08:00",
+  source_note: "公開資訊觀測站重大訊息公告；原文連結採可直接開啟之公告轉載頁。",
+  rows: [
+    {
+      announcement_date: "2026-07-24",
+      company_code: "8936",
+      company_name: "國統",
+      issue_no: "第五次",
+      issue_name: "國內第五次有擔保轉換公司債",
+      guarantee: "有擔保",
+      collection_bank: "台新國際商業銀行敦南分行",
+      deposit_bank: "台新國際商業銀行苓雅分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=4b232ba0-8221-41b0-be57-6b0e81489137"
+    },
+    {
+      announcement_date: "2026-07-24",
+      company_code: "8936",
+      company_name: "國統",
+      issue_no: "第六次",
+      issue_name: "國內第六次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "台新國際商業銀行敦南分行",
+      deposit_bank: "台新國際商業銀行苓雅分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=4b232ba0-8221-41b0-be57-6b0e81489137"
+    },
+    {
+      announcement_date: "2026-07-22",
+      company_code: "3081",
+      company_name: "聯亞",
+      issue_no: "第一次",
+      issue_name: "國內第一次有擔保轉換公司債",
+      guarantee: "有擔保",
+      collection_bank: "元大商業銀行南京東路分行",
+      deposit_bank: "元大商業銀行高雄分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=7e5554cf-b1d3-4837-8584-f73d82cee872"
+    },
+    {
+      announcement_date: "2026-07-21",
+      company_code: "2303",
+      company_name: "聯電",
+      issue_no: "第一次",
+      issue_name: "國內第一次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "華南商業銀行總行營業部",
+      deposit_bank: "中國信託商業銀行敦南分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=cfe541a6-fd6b-45fb-85d8-fd4e6f34c4e9"
+    },
+    {
+      announcement_date: "2026-07-21",
+      company_code: "2303",
+      company_name: "聯電",
+      issue_no: "第二次",
+      issue_name: "國內第二次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "華南商業銀行總行營業部",
+      deposit_bank: "中國信託商業銀行敦南分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=cfe541a6-fd6b-45fb-85d8-fd4e6f34c4e9"
+    },
+    {
+      announcement_date: "2026-07-20",
+      company_code: "3512",
+      company_name: "皇龍",
+      issue_no: "第二次",
+      issue_name: "國內第二次有擔保轉換公司債",
+      guarantee: "有擔保",
+      collection_bank: "台新國際商業銀行敦南分行",
+      deposit_bank: "臺灣銀行六甲頂分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=91e242c0-289e-42df-baa3-804970d9453b"
+    },
+    {
+      announcement_date: "2026-07-17",
+      company_code: "8155",
+      company_name: "博智",
+      issue_no: "第二次",
+      issue_name: "國內第二次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "凱基商業銀行敦北分行",
+      deposit_bank: "中國信託商業銀行城東分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=f41e2243-7180-400e-9386-e0f8275f3287"
+    },
+    {
+      announcement_date: "2026-07-17",
+      company_code: "8155",
+      company_name: "博智",
+      issue_no: "第三次",
+      issue_name: "國內第三次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "凱基商業銀行敦北分行",
+      deposit_bank: "中國信託商業銀行城東分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=f41e2243-7180-400e-9386-e0f8275f3287"
+    },
+    {
+      announcement_date: "2026-07-17",
+      company_code: "5536",
+      company_name: "聖暉*",
+      issue_no: "第二次",
+      issue_name: "國內第二次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "國泰世華商業銀行建成分行",
+      deposit_bank: "凱基商業銀行市政分公司",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=0daaf10d-1be1-4a24-ac85-7aaac480830a"
+    },
+    {
+      announcement_date: "2026-07-17",
+      company_code: "5536",
+      company_name: "聖暉*",
+      issue_no: "第三次",
+      issue_name: "國內第三次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "國泰世華商業銀行建成分行",
+      deposit_bank: "凱基商業銀行市政分公司",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=0daaf10d-1be1-4a24-ac85-7aaac480830a"
+    },
+    {
+      announcement_date: "2026-07-08",
+      company_code: "6903",
+      company_name: "巨漢",
+      issue_no: "第一次",
+      issue_name: "國內第一次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "台北富邦商業銀行敦南分行",
+      deposit_bank: "第一商業銀行新竹分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=d1d550e9-0029-4865-b8ac-c6dbb3fb6b41"
+    },
+    {
+      announcement_date: "2026-07-08",
+      company_code: "6903",
+      company_name: "巨漢",
+      issue_no: "第二次",
+      issue_name: "國內第二次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "台北富邦商業銀行敦南分行",
+      deposit_bank: "第一商業銀行新竹分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=d1d550e9-0029-4865-b8ac-c6dbb3fb6b41"
+    },
+    {
+      announcement_date: "2026-07-06",
+      company_code: "3583",
+      company_name: "辛耘",
+      issue_no: "第三次",
+      issue_name: "國內第三次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "元大商業銀行南京東路分行",
+      deposit_bank: "中國信託商業銀行承德分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=8d092f5d-5f6c-4483-9584-b340850df7c7"
+    },
+    {
+      announcement_date: "2026-07-06",
+      company_code: "3583",
+      company_name: "辛耘",
+      issue_no: "第四次",
+      issue_name: "國內第四次無擔保轉換公司債",
+      guarantee: "無擔保",
+      collection_bank: "華南商業銀行南京東路分行",
+      deposit_bank: "中國信託商業銀行承德分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=8d092f5d-5f6c-4483-9584-b340850df7c7"
+    },
+    {
+      announcement_date: "2026-07-03",
+      company_code: "6869",
+      company_name: "雲豹能源",
+      issue_no: "第二次",
+      issue_name: "國內第二次有擔保轉換公司債",
+      guarantee: "有擔保",
+      collection_bank: "凱基商業銀行敦北分行",
+      deposit_bank: "永豐商業銀行建成分行",
+      source_url: "https://www.moneydj.com/KMDJ/news/newsviewer.aspx?a=0d19c092-a658-472b-96d6-cdc481811593"
+    }
+  ]
+};
